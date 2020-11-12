a5d79bb7344a11d1445b4759040aa3a695b12a04
delta114514 <gey3933@gmail.com>
2020-11-12 12:25:47 +0900

    example gif

--- test/test.py
+++ test/test.py
@@ -12,6 +12,10 @@ class TestJusho(unittest.TestCase):
     def testfrompostal(self):
         self.assertEqual(self.postman.from_postal_code('160-0021').hyphen_postal, '160-0021')
         self.assertEqual(self.postman.address_from_town('大阪府', '三島郡島本町', '青葉', 'kanji').hyphen_postal, '618-0015')
+        kabukicho = self.postman.from_postal_code('160-0021')
+        print(self.postman.prefectures)
+        print(self.postman.cities_from_prefecture('東京都', 'kanji'))
+        print(self.postman.towns_from_city('東京都', '新宿区', 'kanji'))
 
 
 if __name__ == "__main__":
