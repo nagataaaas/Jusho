9705df084d797cef7facaa5a1762d9ea2dd7a1a1
delta114514 <gey3933@gmail.com>
2022-07-19 23:24:46 +0900

    update example/example1

--- example/example1/main.py
+++ example/example1/main.py
@@ -10,57 +10,58 @@ hir2kat = str.maketrans(hiragana_chart, katakana_chart)
 kat2hir = str.maketrans(katakana_chart, hiragana_chart)
 
 
-@api.route('/api/postal')
-async def pref(req, resp):
-    data = await req.media(format='json')
-    user_pref, user_city, user_town = data['pref'], data['city'], data['town']
-    resp.media = {'postal': postman.address_from_town(user_pref, user_city, user_town, 'kanji').hyphen_postal}
+@api.route('/')
+def index(req, resp):
+    resp.html = api.template('index.html')
+
 
 @api.route('/api/pref')
 async def pref(req, resp):
     user_input = (await req.media(format='json'))['pref']
     user_input = user_input.translate(hir2kat).upper()
-    similar = []
-    for pref in postman.prefectures:
-        for val in pref:
-            if val.startswith(user_input):
-                similar.append([pref[1], pref[1]])
-                break
-    resp.media = {'similar': similar}
+    prefs = set()
+    for type_ in ['kanji', 'kana', 'eng']:
+        data = postman.search_prefectures(user_input, type_)
+        if data:
+            prefs.update(data)
+    resp.media = {'similar': [{'name': p.kanji, 'id': p.id} for p in prefs]}
+
 
 @api.route('/api/city')
 async def pref(req, resp):
     data = await req.media(format='json')
-    user_pref, user_city = data['pref'], data['city']
-    user_pref = user_pref.translate(hir2kat).upper()
+    pref_id, user_city = data['pref'], data['city']
+    pref = postman.prefecture_by_id(int(pref_id))
     user_city = user_city.translate(hir2kat).upper()
-    similar = []
-    for city in postman.cities_from_prefecture(user_pref, 'kanji'):
-        for val in city:
-            if val.startswith(user_city):
-                similar.append([city[1], city[1]])
-                break
-    resp.media = {'similar': similar}
+    cities = set()
+    for type_ in ['kanji', 'kana', 'eng']:
+        data = postman.search_cities(user_city, pref, type_)
+        if data:
+            cities.update(data)
+    resp.media = {'similar': [{'name': c.kanji, 'id': c.id} for c in cities]}
+
 
 @api.route('/api/town')
 async def pref(req, resp):
     data = await req.media(format='json')
-    user_pref, user_city, user_town = data['pref'], data['city'], data['town']
-    user_pref = user_pref.translate(hir2kat).upper()
-    user_city = user_city.translate(hir2kat).upper()
+    city_id, user_town = data['city'], data['town']
+    city = postman.city_by_id(int(city_id))
     user_town = user_town.translate(hir2kat).upper()
-    similar = []
-    for town in postman.towns_from_city(user_pref, user_city, 'kanji'):
-        for val in (town.town_area_eng, town.town_area_kanji, town.town_area_kana):
-            if val.startswith(user_town):
-                similar.append([town.town_area_kanji, town.town_area_kanji])
-                break
-    resp.media = {'similar': similar}
+
+    towns = set()
+    for type_ in ['kanji', 'kana', 'eng']:
+        data = postman.search_addresses(user_town, city=city, type_=type_)
+        if data:
+            towns.update(data)
+    resp.media = {'similar': [{'name': t.kanji, 'id': t.id} for t in towns]}
 
 
-@api.route('/')
-def index(req, resp):
-    resp.html = api.template('index.html')
+@api.route('/api/zip')
+async def pref(req, resp):
+    data = await req.media(format='json')
+    town_id = data['town']
+    town = postman.address_by_id(int(town_id))
+    resp.media = {'zip': town.hyphen_zip}
 
 
 if __name__ == '__main__':
